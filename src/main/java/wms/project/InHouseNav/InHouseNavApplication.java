e2f0731b841eafa0d3c48e0ab4f9c3eea0a18ce7
Mrasinthe <marasingherasinthe@gmail.com>
2023-07-05 20:25:22 +0300

    Initial files uploaded

---
+++ src/main/java/wms/project/InHouseNav/InHouseNavApplication.java
@@ -0,0 +1,13 @@
+package wms.project.InHouseNav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InHouseNavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InHouseNavApplication.class, args);
+	}
+
+}
